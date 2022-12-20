0bd2ff2fc63c4afae2dabc0570f517f6506241e2
SergT3 <tigranyan.03@gmail.com>
2022-12-20 17:01:47 +0400

    Minor changes to new_config RawFrame added

---
+++ Frames/raw.py
@@ -0,0 +1,29 @@
+from asciimatics.widgets import Layout, TextBox, Button
+from asciimatics.exceptions import NextScene
+from myframe import MyFrame
+
+
+class RawFrame(MyFrame):
+
+    @staticmethod
+    def get_title():
+        return "Raw"
+
+    def init_layout(self):
+        layout1 = Layout([1, 1, 1, 1])
+        self.add_layout(layout1)
+        layout1.add_widget(Button("Done", on_click=self._cancel), 3)
+        layout2 = Layout([1], True)
+        self.add_layout(layout2)
+        self.text_box = TextBox(self._screen.height // 2, readonly=True, name="textbox")
+        layout2.add_widget(self.text_box)
+        self.fix()
+
+    def _on_load(self):
+        if self._model.get_raw_config():
+            self.text_box.value = self._model._last_config_raw
+        else:
+            pass
+
+    def _cancel(self):
+        raise NextScene("NewConfig")
