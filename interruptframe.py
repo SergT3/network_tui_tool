2ebf10661fc1d0f0bf8a1898b07f07e1a2c8a360
SergT3 <tigranyan.03@gmail.com>
2023-07-14 20:04:50 +0400

    default colour palette added

--- interruptframe.py
+++ interruptframe.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod
 from asciimatics.widgets import Frame
 from asciimatics.exceptions import StopApplication
+from asciimatics.screen import Screen
 from future.utils import with_metaclass
 
 
@@ -13,6 +14,31 @@ class InterruptFrame(with_metaclass(ABCMeta, Frame)):
                                              on_load=self._on_load if hasattr(self, "_on_load") else None,
                                              has_border=True,
                                              title=self.single_title())
+        self.palette = {
+        "background": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+        "shadow": (Screen.COLOUR_BLACK, None, Screen.COLOUR_BLACK),
+        "disabled": (Screen.COLOUR_BLACK, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+        "invalid": (Screen.COLOUR_BLACK, Screen.A_BOLD, Screen.COLOUR_RED),
+        "label": (Screen.COLOUR_GREEN, Screen.A_BOLD, Screen.COLOUR_BLUE),
+        "borders": (Screen.COLOUR_BLACK, Screen.A_BOLD, Screen.COLOUR_BLUE),
+        "scroll": (Screen.COLOUR_GREEN, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+        "title": (Screen.COLOUR_GREEN, Screen.A_BOLD, Screen.COLOUR_BLUE),
+        "edit_text": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+        "focus_edit_text": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_CYAN),
+        "readonly": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLUE),
+        "focus_readonly": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLUE),
+        "button": (Screen.COLOUR_YELLOW, Screen.A_BOLD, Screen.COLOUR_BLUE),
+        "focus_button": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_YELLOW),
+        "control": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+        "selected_control": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLUE),
+        "focus_control": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+        "selected_focus_control": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_CYAN),
+        "field": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+        "selected_field": (Screen.COLOUR_YELLOW, Screen.A_BOLD, Screen.COLOUR_BLUE),
+        "focus_field": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+        "selected_focus_field": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_CYAN),
+    }
+
         self._screen = screen
         self._model = model
         self.init_layout()
